e1d8775dcccb1815ebbede04e0186c3640d99b3a
4Ramkrishna <gs2110045@sgsitsindore.in>
2025-04-05 22:53:15 +0530

    ram ui

--- app/src/main/java/com/example/patientapp/VideoCallActivity.java
+++ app/src/main/java/com/example/patientapp/VideoCallActivity.java
@@ -1,11 +1,14 @@
 package com.example.patientapp;
 
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class VideoCallActivity extends AppCompatActivity {
-
-
-
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_call);
+    }
 }
