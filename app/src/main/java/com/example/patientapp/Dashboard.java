814ce7e62a4388cf48c89f6b3e19ff5818478b0a
4Ramkrishna <gs2110045@sgsitsindore.in>
2025-04-06 19:31:52 +0530

    Video Streaming

--- app/src/main/java/com/example/patientapp/Dashboard.java
+++ app/src/main/java/com/example/patientapp/Dashboard.java
@@ -105,37 +105,36 @@ public class Dashboard extends AppCompatActivity implements BlinkDetectionHelper
     private BlinkDetectionHelper blinkDetectionHelper;
 
     private static final int PERMISSION_REQ_ID = 22;
-    private String myAppId = "e96507b0abbc41969519575c26fa5814";
-    private String channelName = "channel2";
-    private String token = "007eJxTYPC3y/m+U0iDTe2JnvSPJyeffQpcrrX5UJlA+wPmwzdu6MoqMKRampkamCcZJCYlJZsYWppZmhpampqbJhuZpSWaWhiaPLP4kN4QyMhg1PufhZEBAkF8DobkjMS8vNQcIwYGAOdPIWs=";
-    private RtcEngine mRtcEngine;
-    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
-        // Callback when successfully joining the channel
-        @Override
-        public void onJoinChannelSuccess(String channel, int uid, int elapsed) {
-            super.onJoinChannelSuccess(channel, uid, elapsed);
-            showToast("Joined channel " + channel);
-        }
-        // Callback when a remote user or host joins the current channel
-        @Override
-        public void onUserJoined(int uid, int elapsed) {
-            super.onUserJoined(uid, elapsed);
-            runOnUiThread(() -> {
-                // When a remote user joins the channel, display the remote video stream for the specified uid
-                setupRemoteVideo(uid);
-                showToast("User joined: " + uid); // Show toast for user joining
-            });
-        }
-        // Callback when a remote user or host leaves the current channel
-        @Override
-        public void onUserOffline(int uid, int reason) {
-            super.onUserOffline(uid, reason);
-            runOnUiThread(() -> {
-                showToast("User offline: " + uid); // Show toast for user going offline
-            });
-        }
-    };
-
+//    private String myAppId = "e96507b0abbc41969519575c26fa5814";
+//    private String channelName = "channel2";
+//    private String token = "007eJxTYPC3y/m+U0iDTe2JnvSPJyeffQpcrrX5UJlA+wPmwzdu6MoqMKRampkamCcZJCYlJZsYWppZmhpampqbJhuZpSWaWhiaPLP4kN4QyMhg1PufhZEBAkF8DobkjMS8vNQcIwYGAOdPIWs=";
+//    private RtcEngine mRtcEngine;
+//    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
+//        // Callback when successfully joining the channel
+//        @Override
+//        public void onJoinChannelSuccess(String channel, int uid, int elapsed) {
+//            super.onJoinChannelSuccess(channel, uid, elapsed);
+//            showToast("Joined channel " + channel);
+//        }
+//        // Callback when a remote user or host joins the current channel
+//        @Override
+//        public void onUserJoined(int uid, int elapsed) {
+//            super.onUserJoined(uid, elapsed);
+//            runOnUiThread(() -> {
+//                // When a remote user joins the channel, display the remote video stream for the specified uid
+//                setupRemoteVideo(uid);
+//                showToast("User joined: " + uid); // Show toast for user joining
+//            });
+//        }
+//        // Callback when a remote user or host leaves the current channel
+//        @Override
+//        public void onUserOffline(int uid, int reason) {
+//            super.onUserOffline(uid, reason);
+//            runOnUiThread(() -> {
+//                showToast("User offline: " + uid); // Show toast for user going offline
+//            });
+//        }
+//    };
 
 
     @Override
@@ -165,7 +164,7 @@ public class Dashboard extends AppCompatActivity implements BlinkDetectionHelper
         listenForMessages();
         if (checkPermissions()) {
 
-            startVideoCalling();
+//            startVideoCalling();
             blinkDetectionHelper.startCamera(this);
         } else {
             requestPermissions();
@@ -202,52 +201,52 @@ public class Dashboard extends AppCompatActivity implements BlinkDetectionHelper
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_REQ_ID && checkPermissions()) {
 
-            startVideoCalling();
+//            startVideoCalling();
             blinkDetectionHelper.startCamera(this);
         }
     }
-    private void startVideoCalling() {
-        initializeAgoraVideoSDK();
-        enableVideo();
-        setupLocalVideo();
-        joinChannel();
-    }
-    private void initializeAgoraVideoSDK() {
-        try {
-            RtcEngineConfig config = new RtcEngineConfig();
-            config.mContext = getBaseContext();
-            config.mAppId = myAppId;
-            config.mEventHandler = mRtcEventHandler;
-            mRtcEngine = RtcEngine.create(config);
-        } catch (Exception e) {
-            throw new RuntimeException("Error initializing RTC engine: " + e.getMessage());
-        }
-    }
-    private void enableVideo() {
-        mRtcEngine.enableVideo();
-        mRtcEngine.startPreview();
-    }
-    private void setupLocalVideo() {
-//        FrameLayout container = findViewById(R.id.local_video_view_container);
-        SurfaceView surfaceView = new SurfaceView(getBaseContext());
-//        container.addView(surfaceView);
-        mRtcEngine.setupLocalVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_FIT, 0));
-    }
-    private void joinChannel() {
-        ChannelMediaOptions options = new ChannelMediaOptions();
-        options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER;
-        options.channelProfile = Constants.CHANNEL_PROFILE_COMMUNICATION;
-        options.publishCameraTrack = true;
-        options.publishMicrophoneTrack = true;
-        mRtcEngine.joinChannel(token, channelName, 0, options);
-    }
-    private void setupRemoteVideo(int uid) {
-//        FrameLayout container = findViewById(R.id.remote_video_view_container);
-        SurfaceView surfaceView = new SurfaceView(getBaseContext());
-        surfaceView.setZOrderMediaOverlay(true);
-//        container.addView(surfaceView);
-        mRtcEngine.setupRemoteVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_FIT, uid));
-    }
+//    private void startVideoCalling() {
+//        initializeAgoraVideoSDK();
+//        enableVideo();
+//        setupLocalVideo();
+//        joinChannel();
+//    }
+//    private void initializeAgoraVideoSDK() {
+//        try {
+//            RtcEngineConfig config = new RtcEngineConfig();
+//            config.mContext = getBaseContext();
+//            config.mAppId = myAppId;
+//            config.mEventHandler = mRtcEventHandler;
+//            mRtcEngine = RtcEngine.create(config);
+//        } catch (Exception e) {
+//            throw new RuntimeException("Error initializing RTC engine: " + e.getMessage());
+//        }
+//    }
+//    private void enableVideo() {
+//        mRtcEngine.enableVideo();
+//        mRtcEngine.startPreview();
+//    }
+//    private void setupLocalVideo() {
+////        FrameLayout container = findViewById(R.id.local_video_view_container);
+//        SurfaceView surfaceView = new SurfaceView(getBaseContext());
+////        container.addView(surfaceView);
+//        mRtcEngine.setupLocalVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_FIT, 0));
+//    }
+//    private void joinChannel() {
+//        ChannelMediaOptions options = new ChannelMediaOptions();
+//        options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER;
+//        options.channelProfile = Constants.CHANNEL_PROFILE_COMMUNICATION;
+//        options.publishCameraTrack = true;
+//        options.publishMicrophoneTrack = true;
+//        mRtcEngine.joinChannel(token, channelName, 0, options);
+//    }
+//    private void setupRemoteVideo(int uid) {
+////        FrameLayout container = findViewById(R.id.remote_video_view_container);
+//        SurfaceView surfaceView = new SurfaceView(getBaseContext());
+//        surfaceView.setZOrderMediaOverlay(true);
+////        container.addView(surfaceView);
+//        mRtcEngine.setupRemoteVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_FIT, uid));
+//    }
 
     private void initializeViews() {
         mAuth = FirebaseAuth.getInstance();
@@ -368,6 +367,8 @@ public class Dashboard extends AppCompatActivity implements BlinkDetectionHelper
                 controlLights();
             } else if (v == videoCallCard) {
 //                startVideoCall();
+                Intent intent = new Intent(Dashboard.this, LiveStreamService.class);
+                startActivity(intent);
                 return;
             }
 
@@ -663,18 +664,18 @@ public class Dashboard extends AppCompatActivity implements BlinkDetectionHelper
         }
         super.onDestroy();
         blinkDetectionHelper.shutdownCameraExecutor();
-        cleanupAgoraEngine();
+//        cleanupAgoraEngine();
 
 
 
     }
-    private void cleanupAgoraEngine() {
-        if (mRtcEngine != null) {
-            mRtcEngine.stopPreview();
-            mRtcEngine.leaveChannel();
-            mRtcEngine = null;
-        }
-    }
+//    private void cleanupAgoraEngine() {
+//        if (mRtcEngine != null) {
+//            mRtcEngine.stopPreview();
+//            mRtcEngine.leaveChannel();
+//            mRtcEngine = null;
+//        }
+//    }
     private void showToast(String message) {
         runOnUiThread(() -> Toast.makeText(Dashboard.this, message, Toast.LENGTH_SHORT).show());
     }
